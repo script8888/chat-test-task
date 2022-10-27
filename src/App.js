7e39b8311b5cbe4d51639f278b219cd7b7515b60
script8888 <ayomikun204@gmail.com>
2022-10-27 06:31:12 +0100

    implemented chatting function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import { useStoreState } from "easy-peasy";
+import { useEffect, useState } from "react";
+import { useStoreState, useStoreActions } from "easy-peasy";
 import InputName from "./Components/InputName";
 import Chat from "./Components/Chat";
 import "./styles/App.css";
 
 const App = () => {
   const isLoggedIn = useStoreState((state) => state.input.isLoggedIn);
+  const setMessage = useStoreActions((action) => action.chat.setMessages);
+  const chats = useStoreState((state) => state.chat.messages);
+
+  useEffect(() => {
+    const messages = localStorage.messages
+      ? JSON.parse(localStorage.messages)
+      : null;
+
+    if (messages) {
+      setMessage(messages);
+    } else {
+      localStorage.setItem("messages", JSON.stringify([]));
+      setMessage([]);
+    }
+
+    const checkStorage = () => {
+      if (JSON.parse(localStorage.messages).length > chats.length) {
+        setMessage(JSON.parse(localStorage.messages));
+      }
+    };
+
+    window.addEventListener("storage", checkStorage);
+  }, []);
 
   return <>{!isLoggedIn ? <InputName /> : <Chat />}</>;
 };
--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,20 +1,49 @@
 import { useState } from "react";
 import { Input } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 const Chat = () => {
+  const name = useStoreState((action) => action.input.name);
+  const messages = useStoreState((action) => action.chat.messages);
+  const setMessage = useStoreActions((action) => action.chat.setMessages);
+
   const [userMessage, setUserMessage] = useState("");
+
+  const createNewMessage = (e) => {
+    e.preventDefault();
+
+    const newText = {
+      id: Math.random(),
+      text: userMessage,
+      name: name,
+    };
+
+    const newMessageArray = [...messages, newText];
+
+    setMessage(newMessageArray);
+
+    localStorage.setItem("messages", JSON.stringify(newMessageArray));
+
+    setUserMessage("");
+  };
 
   return (
     <div className="chat_parent">
       <div className="chat_container">
         <div className="chat_message_container">
           <div className="chat_messageGrandParent">
+            {messages.map((data) => (
               <div
+                key={data.id}
                 className="chat_messageParent"
+                style={{
+                  alignSelf: data.name === name ? "flex-end" : "flex-start",
+                  backgroundColor: data.name === name ? "green" : "firebrick",
+                }}
               >
-                <div className="chat_message_user">name</div>
-                <div className="chat_message_text">user</div>
+                <div className="chat_message_user">{data.name}</div>
+                <div className="chat_message_text">{data.text}</div>
               </div>
             ))}
           </div>
@@ -27,7 +56,7 @@ const Chat = () => {
             placeholder="message..."
             onInput={(e) => setUserMessage(e.target.value)}
           />
-          <Button colorScheme="teal" size="sm">
+          <Button onClick={createNewMessage} colorScheme="teal" size="sm">
             Send
           </Button>
         </div>
